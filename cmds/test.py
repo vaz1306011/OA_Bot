a1f5266f95784eea907ec739b6554a63067f142e
vaz1306011 <vaz1306011@gmail.com>
2022-09-10 15:13:57 +0800

    新增test模塊

---
+++ cmds/test.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+from core.classes import Cog_Extension
+from core.tools import ctx_send
+
+
+class Test(Cog_Extension):
+    @commands.command()
+    async def test(self, ctx):
+        await ctx_send(ctx, "red", color="r")
+        await ctx_send(ctx, "orange", color="o")
+        await ctx_send(ctx, "yellow", color="y")
+        await ctx_send(ctx, "green", color="g")
+        await ctx_send(ctx, "lightGreen", color="lg")
+        await ctx_send(ctx, "blue", color="b")
+
+    @commands.command()
+    async def test2(self, ctx):
+        game = discord.Game(name="吸娜娜奇")
+        await self.bot.change_presence(status=discord.Status.idle, activity=game)
+
+
+def setup(bot):
+    print("已讀取Test")
+    bot.add_cog(Test(bot))
+
+
+def teardown(bot):
+    print("已移除Test")
+    bot.remove_cog("Test")
