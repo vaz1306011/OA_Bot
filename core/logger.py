1c280c8d0043897cbfd5fb94bf859a45f86c9702
小飛 <vaz1306011@gmail.com>
2024-08-15 07:24:57 +0800

    修改 log格式
    秒數只保留整數部分

--- core/logger.py
+++ core/logger.py
@@ -3,5 +3,7 @@ import logging
 logger = logging.getLogger("OA_Bot")
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
+)
 logger.addHandler(handler)
